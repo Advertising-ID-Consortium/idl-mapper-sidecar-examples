1f7a5b184b365babb76e993df26a73faafb5dd81
Nikola Kološnjaji <n.kolosnjaji@levi9.com>
2020-02-21 16:00:05 +0100

    Call health endpoint

--- unix-domain-socket-simple-java-client/src/main/java/com/liveramp/sidecar/client/UnixDomainSocketExample.java
+++ unix-domain-socket-simple-java-client/src/main/java/com/liveramp/sidecar/client/UnixDomainSocketExample.java
@@ -11,8 +11,10 @@ import org.newsclub.net.unix.AFUNIXSocketAddress;
 public class UnixDomainSocketExample {
 
   private static final String LIVERAMP_IDL_MAPPER_SOCKET = "/tmp/liveramp/idl-mapper-socket";
-  private static final String LIVERAMP_IDL_MAPPER_MESSAGE = "GET /map?env=AnZGl_biYwUNkglgfKZiXMLv1YEYkzTFzkgTIQ17F_i3l4ruMEzWNJDDm1Ofv7c0ynB51uFQO9ytmMGuDX8N8I89s2s HTTP/1.1\n"
+  private static final String LIVERAMP_IDL_MAPPER_HEALTH_MSG = "GET /health HTTP/1.1\nHost: localhost\n\n";
+  private static final String LIVERAMP_IDL_MAPPER_MAP_MSG = "GET /map?env=AnZGl_biYwUNkglgfKZiXMLv1YEYkzTFzkgTIQ17F_i3l4ruMEzWNJDDm1Ofv7c0ynB51uFQO9ytmMGuDX8N8I89s2s HTTP/1.1\n"
       + "Host: localhost\n\n";
+  
 
   public static void main(String[] args) throws IOException {
 
@@ -24,16 +26,27 @@ public class UnixDomainSocketExample {
       try (InputStream is = sock.getInputStream(); //
           OutputStream os = sock.getOutputStream();) {
 
-        os.write(LIVERAMP_IDL_MAPPER_MESSAGE.getBytes(StandardCharsets.UTF_8));
+        os.write(LIVERAMP_IDL_MAPPER_HEALTH_MSG.getBytes(StandardCharsets.UTF_8));
         os.flush();
         
-        int bytes;
         byte[] readBuf = new byte[sock.getReceiveBufferSize()];
+        int bytes = is.read(readBuf);
+        
+        System.out.println("Response from Sidecar's '/health' endpoint:");
+        System.out.println();
+        System.out.println(new String(readBuf, 0, bytes, StandardCharsets.UTF_8));
 
-        while ((bytes = is.read(readBuf)) != -1) {
-          System.out.write(readBuf, 0, bytes);
-          System.out.flush();
-        }
+        os.write(LIVERAMP_IDL_MAPPER_MAP_MSG.getBytes(StandardCharsets.UTF_8));
+        os.flush();
+        
+        readBuf = new byte[sock.getReceiveBufferSize()];
+        bytes = is.read(readBuf);
+        
+        System.out.println();
+        System.out.println("Response from Sidecar's '/env' endpoint:");
+        System.out.println();
+        System.out.println(new String(readBuf, 0, bytes, StandardCharsets.UTF_8));
+        
       }
     }
   }
